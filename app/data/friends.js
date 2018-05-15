16f35b8b212c73ba0761c3ee3b7b7989f61190a5
Brenda C. Cheung <bcc8706@fsm.northwestern.edu>
2018-05-14 23:41:09 -0500

    Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friendsArray = [
+    {
+        "name": "Jenny",
+        "photo": "http://localhost:3000/images/Jenny_freeimage-21175804-web.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "http://localhost:3000/images/John_freeimage-21182670-web.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Kathleen",
+        "photo": "http://localhost:3000/images/Kathleen_freeimage-29592991-web.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Maryln",
+        "photo": "http://localhost:3000/images/Maryln_freeimage-20850199-web.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Nancy",
+        "photo": "http://localhost:3000/images/Nancy_freeimage-36343520-web.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            5
+        ]
+    },
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
+
